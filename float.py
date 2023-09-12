5c018eea5b6b64e36dc56206603cd3e7806d9164
Edward <eboronka@gmail.com>
2023-09-12 12:55:30 -0400

    sorting algorithm
    1

--- float.py
+++ float.py
@@ -1,18 +1,22 @@
 #get a string of numbers from the user and convert it to a list of those numbers in the order it was writen in  # noqa: E501
 
-num_list = input('what is your list of 5 numbers you want to sort? ')
-num_list = [*num_list]
-num_list = [eval(i)for i in num_list]
+num_list =input('what is your list of numbers you want to sort? ')
+num_list = 
+
 
 #sort the list of numbers from least to greatest
 def insetion_sort(num_list):
   
   for i in range(1,len(num_list)):
     j =i
-    while num_list
+    while num_list[j-1]> num_list[j] and j>0:
+      num_list[j-1], num_list[j] = num_list[j], num_list[j-1]
+      j-=1
+      
+      
     
     
-  
+insetion_sort(num_list)
 
   
 
