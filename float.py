7d179ee4e2f15568264780a19b57342c3f391f8f
Edward <eboronka@gmail.com>
2023-09-11 22:10:24 -0400

    making my own algorithim to sort a list of numbers
    1

---
+++ float.py
@@ -0,0 +1,17 @@
+x1 = int(input("what is x1 "))
+
+x2 = int(input("what is x2 "))
+
+x3 = int(input("what is x3 "))
+
+x4 = int(input("what is x4 "))
+
+x5 = int(input("what is x5 "))
+
+
+list =[x1,x2,x3,x4,x5]
+print(list)
+
+def sort_list():
+    if x2>x1:
+      insert()  
